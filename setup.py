046e2625089d32acaba920fd43e9c968dc13ea2e
arnavg115 <73305591+arnavg115@users.noreply.github.com>
2021-08-27 23:32:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="rz-colorization",
-    version="1.0.0",
+    version="1.0.1",
     description="Rich Zhang's colorization model in the form of an easy to use python package.",
     long_description=README,
     long_description_content_type="text/markdown",
